0539a26d7615868639e9badb96cf03a178fc8a3f
Dmitry Elagin <dmitry.elagin@gmail.com>
2016-06-05 20:16:03 +0400

    Added test code which is not working
    Will try to resolve security issue of tainting canvas.

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -11,9 +11,8 @@ Sprite = function(setup) {
 
   var ratio;
 
-  this.image = (function(img) {
-    return img;
-  })(images[setup.imgID]);
+  this.image = images[setup.imgID];
+  //this.colorize(setup.color || '000000');
 
   ratio = Math.max(setup.container.width, setup.container.height) /
       Math.max(this.image.width, this.image.height) || 1;
@@ -33,6 +32,47 @@ Sprite = function(setup) {
   this.dy = -this.height / 2;
 
   Object.freeze(this);
+
+};
+
+Sprite.prototype = {
+
+  constructor: TRIPLET.Sprite,
+
+  colorize: function(colorStr) {
+
+    var canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        colorToSet = [
+          hex2int(colorStr.substr(0, 2)),  // Red
+          hex2int(colorStr.substr(2, 2)),  // Green
+          hex2int(colorStr.substr(4, 2))   // Blue
+        ],
+        i, imgData;
+
+    function hex2int(str) {
+      return parseInt(str, 16) || 0;
+    }
+
+    function makeImageData(img) {
+      img.crossOrigin = 'anonymous';
+      context.drawImage(img, 0, 0);
+      return context.getImageData(0, 0, img.width, img.height);
+    }
+
+    function saveNewImage(self) {
+      var img = new Image();
+      context.putImageData(imgData, 0, 0);
+      img.src = canvas.toDataURL('image/png');
+      img.onload = function() { self.image = img; };
+    }
+
+    imgData = makeImageData(this.image);
+    for (i = imgData.data.length; i--;)
+      if (i % 4 !== 3) imgData.data[i] = colorToSet[i % 4];
+    saveNewImage(this);
+
+  }
 
 };
 
