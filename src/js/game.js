bed141ad3e786f83b4b449a7eaeec94c24295838
Dmitry Elagin <dmitry.elagin@gmail.com>
2016-06-09 17:30:40 +0300

    Module Game converted to ES2015
    Now it has comment for JSHint, that should be removed.

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 // TODO Making canvas and worker is not in config
 // TODO Add timings in action
 // TODO Ask user to wait or terminate if user clicks while AI working
@@ -7,83 +8,74 @@
 // TODO Maybe partsToLoad should be dynamic or private number
 // TODO Many values should be in config
 // Game main presenter
-TRIPLET.Game = (function() {
+import cfg from 'config';
+import assets from 'assets';
+import html from 'html';
+import worker from 'worker';
+import { makeWorker } from 'utilities';
+import Field from 'field';
+import Picture from 'picture';
 
-var cfg = TRIPLET.config,
-    assets = TRIPLET.assets,
-    html = TRIPLET.html,
-    worker = TRIPLET.worker,
-    ufn = TRIPLET.utilities.function,
-    Field = TRIPLET.Field,
-    State = TRIPLET.State,
-    Picture = TRIPLET.Picture,
-    Game;
+export default class Game {
 
-Game = function(id) {
+  constructor (id = 0) {
+    const startGame = () => {
+      if (--partsToLoad) return;
+      this.picture.drawField();
+      this.canvas.addEventListener('click', this.onClick.bind(this));
+      this.action();
+    };
+    let partsToLoad = 3;
+    this.userTurn = false;
 
-  var self = this,
-      partsToLoad = 3;
-  this.userTurn = false;
+    assets.images.load(cfg.assets.images, startGame);
+    this.state = makeWorker({
+      code: worker,
+      onload: startGame,
+      handler: this.respond.bind(this),
+      importFrom: document.location.href.replace(/[^\/]*$/, '') + 'js' + '/'
+    });
 
-  function startGame() {
-    if (--partsToLoad) return;
-    self.picture.drawField();
-    self.canvas.addEventListener('click', self.onClick.bind(self));
-    self.action();
+    this.field = new Field();
+    this.canvas = html.makeCanvas(
+        'triplet-' + id,
+        cfg.general.left + cfg.general.right + this.field.width,
+        cfg.general.top + cfg.general.bottom + this.field.height,
+        document.getElementsByTagName('body')[0]);
+    this.picture = new Picture(this.field, this.canvas);
+
+    startGame();
   }
 
-  assets.images.load(cfg.assets.images, startGame);
-  this.state = ufn.makeWorker({
-    code: worker,
-    onload: startGame,
-    handler: this.respond.bind(this),
-    importFrom: document.location.href.replace(/[^\/]*$/, '') + 'js' + '/'
-  });
-
-  this.field = new Field();
-  this.canvas = html.makeCanvas(
-      'triplet-' + id,
-      cfg.general.left + cfg.general.right + this.field.width,
-      cfg.general.top + cfg.general.bottom + this.field.height,
-      document.getElementsByTagName('body')[0]);
-  this.picture = new Picture(this.field, this.canvas);
-
-  startGame();
-
-};
-
-Game.prototype = {
-
-  constructor: Game,
-
-  onClick: function(event) {
-    var coords = html.getClickCoords(event);
+  onClick (e) {
     if (this.userTurn) {
       this.userTurn = false;
-      this.tryMove(this.field.getCellPosition(coords.x, coords.y));
+      let { x, y } = html.getClickCoords(e);
+      this.tryMove(this.field.getCellPosition(x, y));
     } else {
       console.log('Please wait for your turn.');
     }
-  },
+  }
 
-  respond: function(message) {
+  respond (message) {
     console.log('time: ' + message.data.aiSpeed);
     if (message.data.bestMove) {
       this.tryMove(message.data.bestMove);
       console.log('minimax score: ' + message.data.bestMove.score[0]);
     }
     else this.action(message.data);
-  },
+  }
 
-  tryMove: function(cell) {
+  tryMove (cell) {
     this.state.postMessage({ move: cell });
-  },
+  }
 
-  action: function(result) {
+  action (result) {
     if (result) {
-      if (result.success)
-        this.picture.drawSign(result.lastMove.row, result.lastMove.col,
-                              result.lastMove.player);
+      if (result.success) {
+        let { row, col, player } = result.lastMove;
+        this.picture.drawSign(row, col, player);
+      }
       if (result.player.isUser) {
         this.userTurn = true;
         console.log('User turn.');
@@ -98,8 +90,4 @@ Game.prototype = {
     }
   }
 
-};
-
-return Game;
-
-})();
+}
