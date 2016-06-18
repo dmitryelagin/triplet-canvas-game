2f01fe70fb5cad8fb231169ad65e16b49a35e4e5
Dmitry Elagin <dmitry.elagin@gmail.com>
2016-06-18 16:56:38 +0400

    ES5 files are rebuilded
    Also gulpfile was corrected to convert main entry point.

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,12 @@
+'use strict';
+
 // TODO Add multi-game feature
-const requireCfg = {
+var requireCfg = {
   baseUrl: 'js/lib',
-  paths: { app: '../app' },
+  paths: { app: '../app' }
 };
-requirejs(requireCfg, ['jquery', 'app/game'], ($, Game) => {
-  const games = [];
+requirejs(requireCfg, ['jquery', 'app/game'], function ($, Game) {
+  var games = [];
   games.unshift(new Game(games.length, requireCfg));
 });
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJqcy9tYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIFRPRE8gQWRkIG11bHRpLWdhbWUgZmVhdHVyZVxyXG5jb25zdCByZXF1aXJlQ2ZnID0ge1xyXG4gIGJhc2VVcmw6ICdqcy9saWInLFxyXG4gIHBhdGhzOiB7IGFwcDogJy4uL2FwcCcgfSxcclxufTtcclxucmVxdWlyZWpzKHJlcXVpcmVDZmcsIFsnanF1ZXJ5JywgJ2FwcC9nYW1lJ10sICgkLCBHYW1lKSA9PiB7XHJcbiAgY29uc3QgZ2FtZXMgPSBbXTtcclxuICBnYW1lcy51bnNoaWZ0KG5ldyBHYW1lKGdhbWVzLmxlbmd0aCwgcmVxdWlyZUNmZykpO1xyXG59KTtcclxuIl0sImZpbGUiOiJqcy9tYWluLmpzIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImpzL21haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQ0EsSUFBTSxhQUFhO0FBQ2pCLFdBQVMsUUFEUTtBQUVqQixTQUFPLEVBQUUsS0FBSyxRQUFQO0FBRlUsQ0FBbkI7QUFJQSxVQUFVLFVBQVYsRUFBc0IsQ0FBQyxRQUFELEVBQVcsVUFBWCxDQUF0QixFQUE4QyxVQUFDLENBQUQsRUFBSSxJQUFKLEVBQWE7QUFDekQsTUFBTSxRQUFRLEVBQWQ7QUFDQSxRQUFNLE9BQU4sQ0FBYyxJQUFJLElBQUosQ0FBUyxNQUFNLE1BQWYsRUFBdUIsVUFBdkIsQ0FBZDtBQUNELENBSEQiLCJmaWxlIjoianMvbWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIFRPRE8gQWRkIG11bHRpLWdhbWUgZmVhdHVyZVxyXG5jb25zdCByZXF1aXJlQ2ZnID0ge1xyXG4gIGJhc2VVcmw6ICdqcy9saWInLFxyXG4gIHBhdGhzOiB7IGFwcDogJy4uL2FwcCcgfSxcclxufTtcclxucmVxdWlyZWpzKHJlcXVpcmVDZmcsIFsnanF1ZXJ5JywgJ2FwcC9nYW1lJ10sICgkLCBHYW1lKSA9PiB7XHJcbiAgY29uc3QgZ2FtZXMgPSBbXTtcclxuICBnYW1lcy51bnNoaWZ0KG5ldyBHYW1lKGdhbWVzLmxlbmd0aCwgcmVxdWlyZUNmZykpO1xyXG59KTtcclxuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,16 @@
-// TODO Delete obsolete comments when needed
 import gulp from 'gulp';
 import sourcemaps from 'gulp-sourcemaps';
 import babel from 'gulp-babel';
 import gulpIf from 'gulp-if';
 import del from 'del';
 
-const dir = { src: 'src', dest: 'build', app: 'app' };
+const dir = { src: 'src', dest: 'build', app: 'app', main: 'js' };
 
 gulp.task('build', () => gulp.src(`${dir.src}/**/*.*`)
     .pipe(gulpIf(f => f.extname.match(/(?:js|css)/), sourcemaps.init()))
-    .pipe(gulpIf(f => f.dirname.match(`(${dir.app})$`), babel({
+    .pipe(gulpIf(f => f.dirname.match(`(${dir.app}|${dir.main})$`), babel({
       presets: ['es2015'],
-      plugins: [
-        // 'transform-es2015-modules-amd',
-        'transform-exponentiation-operator',
-      ],
+      plugins: ['transform-exponentiation-operator'],
     })))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(dir.dest)));
